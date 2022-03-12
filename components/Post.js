2c9899686b9ff5f3bb924d65db44e4f71423b3af
hugues leger <hugueslegerdevweb@gmail.com>
2022-03-12 12:43:52 +0100

    comment post

--- components/Post.js
+++ components/Post.js
@@ -1,50 +1,40 @@
-import Link from "next/link";
-import Image from "next/image";
-//to use Image with an external url, add some config on next.config.js
-//for more info, check out these docs https://nextjs.org/docs/basic-features/image-optimization
+// import Link from "next/link";
+// import Image from "next/image";
+// //to use Image with an external url, add some config on next.config.js
+// //for more info, check out these docs https://nextjs.org/docs/basic-features/image-optimization
 
-import { getDate } from "../utils/utils";
+// import { getDate } from "../utils/utils";
 
-export default function Post({ post, featuredMedia }) {
-  return (
-    <div className="card mb-3" style={{ maxWidth: "540px" }}>
-      <div className="row g-0">
-        <div className="col-md-4">
-          {/* <Link href={`/posts/${post.slug}`}>
-            <a>
-              <Image
-                src={featuredMedia["media_details"].sizes.medium["source_url"]}
-                width={180}
-                height={120}
-                alt={featuredMedia["alt_text"]}
-              />
-            </a>
-          </Link> */}
-          <Image
-            src={featuredMedia["media_details"].sizes.medium["source_url"]}
-            width={600}
-            height={400}
-            alt={featuredMedia["alt_text"]}
-            className="thumb"
-          />
-        </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <h5 className="card-title">{post.title.rendered}</h5>
-            <div
-              className="card-text"
-              dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
-            ></div>
-            <p>{post.acf.titre}</p>
-            <p className="card-text">
-              <small className="text-muted">Le {getDate(post.modified)}</small>
-            </p>
-            <Link href={`/posts/${post.slug}`}>
-              <a className="btn btn-primary">See more</a>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+// export default function Post({ post, featuredMedia }) {
+//   return (
+//     <div className="card mb-3" style={{ maxWidth: "540px" }}>
+//       <div className="row g-0">
+//         <div className="col-md-4">
+//           <Image
+//             src={featuredMedia["media_details"].sizes.medium["source_url"]}
+//             width={600}
+//             height={400}
+//             alt={featuredMedia["alt_text"]}
+//             className="thumb"
+//           />
+//         </div>
+//         <div className="col-md-8">
+//           <div className="card-body">
+//             <h5 className="card-title">{post.title.rendered}</h5>
+//             <div
+//               className="card-text"
+//               dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+//             ></div>
+//             <p>{post.acf.titre}</p>
+//             <p className="card-text">
+//               <small className="text-muted">Le {getDate(post.modified)}</small>
+//             </p>
+//             <Link href={`/posts/${post.slug}`}>
+//               <a className="btn btn-primary">See more</a>
+//             </Link>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
