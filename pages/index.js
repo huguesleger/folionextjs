0b1a15b4c589983e449fde6efce620403b724250
hugues leger <hugueslegerdevweb@gmail.com>
2022-03-12 12:41:12 +0100

    test vercel

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,27 @@ import Post from "../components/Post";
 import { getPosts } from "../utils/wordpress";
 
 const Home = ({ posts }) => {
-  const jsxPosts = posts.map((post) => {
-    const featuredMedia = post["_embedded"]["wp:featuredmedia"][0];
-    return <Post post={post} featuredMedia={featuredMedia} key={post.id} />;
-  });
+  // const jsxPosts = posts.map((post) => {
+  //   const featuredMedia = post["_embedded"]["wp:featuredmedia"][0];
+  //   return <Post post={post} featuredMedia={featuredMedia} key={post.id} />;
+  // });
 
   return (
     <div className="homepage">
       <div>hello</div>
-      <div className="post">{jsxPosts}</div>
+      {/* <div className="post">{jsxPosts}</div> */}
     </div>
   );
 };
 
 export default Home;
 
-export async function getStaticProps({ params }) {
-  const posts = await getPosts();
-  return {
-    props: {
-      posts,
-    },
-    revalidate: 10, // In seconds
-  };
-}
+// export async function getStaticProps({ params }) {
+//   const posts = await getPosts();
+//   return {
+//     props: {
+//       posts,
+//     },
+//     revalidate: 10, // In seconds
+//   };
+// }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,59 +1,59 @@
-import Link from "next/link";
-import Image from "next/image";
+// import Link from "next/link";
+// import Image from "next/image";
 
-import { getPost, getSlugs } from "../../utils/wordpress";
+// import { getPost, getSlugs } from "../../utils/wordpress";
 
-export default function PostPage({ post }) {
-  return (
-    <>
-      <div className="container pt-5">
-        <h1 className="text-center pb-5">{post.title.rendered}</h1>
-        <Image
-          className="img-fluid"
-          src={
-            post._embedded["wp:featuredmedia"][0].media_details.sizes.full
-              .source_url
-          }
-          width={180}
-          height={120}
-          layout="responsive"
-        />
-        <div className="acf">
-          <p>{post.acf.titre}</p>
-        </div>
-        <div
-          className="card-text pb-5"
-          dangerouslySetInnerHTML={{ __html: post.content.rendered }}
-        ></div>
-        <Link href="/">
-          <a className="btn btn-primary">Back to Home</a>
-        </Link>
-      </div>
-    </>
-  );
-}
+// export default function PostPage({ post }) {
+//   return (
+//     <>
+//       <div className="container pt-5">
+//         <h1 className="text-center pb-5">{post.title.rendered}</h1>
+//         <Image
+//           className="img-fluid"
+//           src={
+//             post._embedded["wp:featuredmedia"][0].media_details.sizes.full
+//               .source_url
+//           }
+//           width={180}
+//           height={120}
+//           layout="responsive"
+//         />
+//         <div className="acf">
+//           <p>{post.acf.titre}</p>
+//         </div>
+//         <div
+//           className="card-text pb-5"
+//           dangerouslySetInnerHTML={{ __html: post.content.rendered }}
+//         ></div>
+//         <Link href="/">
+//           <a className="btn btn-primary">Back to Home</a>
+//         </Link>
+//       </div>
+//     </>
+//   );
+// }
 
-//hey Next, these are the possible slugs
-export async function getStaticPaths() {
-  const paths = await getSlugs("posts");
+// //hey Next, these are the possible slugs
+// export async function getStaticPaths() {
+//   const paths = await getSlugs("posts");
 
-  return {
-    paths,
-    //this option below renders in the server (at request time) pages that were not rendered at build time
-    //e.g when a new blogpost is added to the app
-    fallback: "blocking",
-  };
-}
+//   return {
+//     paths,
+//     //this option below renders in the server (at request time) pages that were not rendered at build time
+//     //e.g when a new blogpost is added to the app
+//     fallback: "blocking",
+//   };
+// }
 
-//access the router, get the id, and get the data for that post
+// //access the router, get the id, and get the data for that post
 
-export async function getStaticProps({ params }) {
-  const post = await getPost(params.slug);
+// export async function getStaticProps({ params }) {
+//   const post = await getPost(params.slug);
 
-  return {
-    props: {
-      post,
-    },
-    revalidate: 10, // In seconds
-  };
-}
+//   return {
+//     props: {
+//       post,
+//     },
+//     revalidate: 10, // In seconds
+//   };
+// }
