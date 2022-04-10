268af1565a6707fbc19240b6ebb972a7ace63f54
hugues leger <hugueslegerdevweb@gmail.com>
2022-04-10 10:25:05 +0200

    test swiper

--- pages/projets/index.tsx
+++ pages/projets/index.tsx
@@ -26,52 +26,13 @@ const ProjetPage: NextPage = (props) => {
     <>
       <div className="projects">
         <div className="wrap-slider">
-          <Swiper
+          {/* <Swiper
             spaceBetween={150}
-            // spaceBetween={0}
             slidesPerView={3}
             centeredSlides={true}
             loop={true}
-            // roundLengths={true}
-            // loopAdditionalSlides={30}
             autoHeight={false}
             grabCursor={true}
-            // onTouchStart={() => {
-            //   document.querySelector(".swiper").classList.add("is-draggy");
-            // }}
-            // onTouchEnd={() =>
-            //   document.querySelector(".swiper").classList.remove("is-draggy")
-            // }
-            // onTransitionStart={() => {
-            //   document.querySelector(".swiper").classList.add("is-draggy");
-            // }}
-            // onTransitionEnd={() =>
-            //   document
-            //     .querySelector(".swiper-slide-active")
-            //     .classList.add("is-hover")
-            // }
-            // onSlideChangeTransitionStart={() => {
-            //   const elActives: any =
-            //     document.querySelectorAll(".swiper-slide a");
-
-            //   elActives.forEach((elActive) => {
-            //     elActive.style.pointerEvents = "none";
-            //   });
-            // }}
-            // onSlideChangeTransitionEnd={() => {
-            //   const elActives: any = document.querySelectorAll(
-            //     ".swiper-slide-active a"
-            //   );
-
-            //   elActives.forEach((elActive) => {
-            //     elActive.style.pointerEvents = "initial";
-            //   });
-            // }}
-            // on={() => {
-            //   // document
-            //   //   .querySelector(".swiper-slide-active")
-            //   //   .classList.add("is-hover");
-            // }}
           >
             {projets.map((projet) => {
               return (
@@ -90,9 +51,7 @@ const ProjetPage: NextPage = (props) => {
                           objectFit="cover"
                           quality={100}
                           alt="montpellier"
-                          // priority
                         />
-                        {/* <img src={projet.imageHeader.url} alt="montpellier" /> */}
                       </div>
                       <div className="outer-title">
                         <div className="name">
@@ -104,7 +63,34 @@ const ProjetPage: NextPage = (props) => {
                 </SwiperSlide>
               );
             })}
-          </Swiper>
+          </Swiper> */}
+          {projets.map((projet) => {
+            return (
+              <Link key={projet.slug} href={"/projets/" + projet.slug}>
+                <a>
+                  <div className="inner-title title-white">
+                    <span className="name">
+                      <span>{projet.titre}</span>
+                    </span>
+                  </div>
+                  <div className="inner-img">
+                    <Image
+                      src={projet.imageHeader.url}
+                      layout="fill"
+                      objectFit="cover"
+                      quality={100}
+                      alt="montpellier"
+                    />
+                  </div>
+                  <div className="outer-title">
+                    <div className="name">
+                      <span>{projet.titre}</span>
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            );
+          })}
         </div>
       </div>
     </>
