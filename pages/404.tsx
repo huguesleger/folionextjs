2a44b7a7067ec4ebef43f8be53e7c5f37119add5
hugues leger <hugueslegerdevweb@gmail.com>
2022-09-18 12:22:19 +0200

    add page error 404 + component changeWord

---
+++ pages/404.tsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import React, { useContext, useEffect } from "react";
+import ChangeWord from "../components/ChangeWord";
+import { Context } from "../context/AppContext";
+
+const Error: () => JSX.Element = () => {
+  const { setNavBar } = useContext(Context);
+
+  useEffect(() => {
+    setNavBar(false);
+  });
+
+  return (
+    <div className="page-error">
+      <div className="error-infos">
+        <ChangeWord
+          word1={"Oups !"}
+          word2={"C'est la mouise !"}
+          word3={"Mince !"}
+          word4={"F*** !"}
+        />
+        <p>une erreur est survenue</p>
+      </div>
+      <div className="infinite-keywords">
+        <p className="keywords">
+          <span className="wrap-keyword keyword-bold">
+            404
+            <span className="separate-keyword">
+              <i className="fas fa-circle" aria-hidden></i>
+            </span>
+          </span>
+          <span className="wrap-keyword">
+            Cette page n'existe pas
+            <span className="separate-keyword">
+              <i className="fas fa-circle" aria-hidden></i>
+            </span>
+          </span>
+        </p>
+        <p className="keywords">
+          <span className="wrap-keyword keyword-bold">
+            404
+            <span className="separate-keyword">
+              <i className="fas fa-circle" aria-hidden></i>
+            </span>
+          </span>
+          <span className="wrap-keyword">
+            Cette page n'existe pas
+            <span className="separate-keyword">
+              <i className="fas fa-circle" aria-hidden></i>
+            </span>
+          </span>
+        </p>
+      </div>
+      <div className="link-home">
+        <Link href="/">
+          <a className="btn-around bg-dark-grey btn-effect">
+            <span>
+              <span>
+                <i className="fas fa-home" aria-hidden></i>
+                Retour à l'accueil
+              </span>
+            </span>
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
