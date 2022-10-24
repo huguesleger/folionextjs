515dc215375f45ed33a7ba8cee5e40a3522d3457
hugues leger <hugueslegerdevweb@gmail.com>
2022-10-24 22:53:38 +0200

    fix delay transition bg color

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ const Home: (props: { home: GraphQLResponse.Home }) => JSX.Element = (props: {
     tl.to(refHome.current, {
       backgroundColor: "#ffffff",
       ease: "Power2.easeInOut",
-      delay: 0.5,
+      delay: 0.8,
       duration: 0.5,
       onStart: () => {
         refHome.current.classList.remove("bg-dark");
