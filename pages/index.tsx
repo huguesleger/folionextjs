916e44b51a19683869ab3148fa78e246486945ae
hugues leger <hugueslegerdevweb@gmail.com>
2022-10-24 22:51:47 +0200

    change duratation transition bg

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,7 +53,7 @@ const Home: (props: { home: GraphQLResponse.Home }) => JSX.Element = (props: {
       backgroundColor: "#ffffff",
       ease: "Power2.easeInOut",
       delay: 0.5,
-      duration: 0.3,
+      duration: 0.5,
       onStart: () => {
         refHome.current.classList.remove("bg-dark");
       },
